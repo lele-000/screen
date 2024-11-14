7424ffbb73f1db716805ed577c8778f8c159b69a
lele-000 <a1219128243@gmail.com>
2024-11-14 16:22:40 +0800

    new

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 const process = require("child_process")
 const WebSocket = require("ws")
 
-const wss = new WebSocket.Server({port:8080})
-wss.on("connection",(ws)=>{
+const wss = new WebSocket.Server({ port: 8080 })
+wss.on("connection", (ws) => {
     console.log("client connected")
-    const sendScreenshot = () => {
-        const adb = process.spawn('adb',['exec-out','screencap','-p'])
-        let chunks = []
-        adb.stdout.on("data",(data)=>{
-            chunks.push(data)
-        })
-        adb.on("close",()=>{
-            const buffer = Buffer.concat(chunks)
-            const base64Image = buffer.toString('base64')
-            ws.send(base64Image)
-        })
-        adb.on("error",(error)=>{
-            console.error("no capture",error)
-        })
-    }
-    const intervalId = setInterval(sendScreenshot,100)
-    ws.on("close",()=>{
-        clearInterval(intervalId);
+    const adb = process.spawn('adb', ['exec-out', 'screenrecord', '--output-format', 'mp4', '-'])
+    const ffmpeg = process.spawn('ffmpeg', [
+        '-i', 'pipe:0',
+        '-f', 'mjpeg',
+        '-q:v', '5',
+        'pipe:1'
+    ])
+    adb.stdout.pipe(ffmpeg.stdin)
+    ffmpeg.stdout.on('data', (data) => {
+        ws.send(data)
+    })
+
+    ws.on("close", () => {
+        adb.kill()
+        ffmpeg.kill()
         console.log("client disconnected")
     })
 })
