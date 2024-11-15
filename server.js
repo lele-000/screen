bf5e6dbf873e3879d4b8312e3d131af02f8766db
lele-000 <a1219128243@gmail.com>
2024-11-15 22:26:36 +0800

    new

--- server.js
+++ server.js
@@ -4,20 +4,23 @@ const WebSocket = require("ws")
 const wss = new WebSocket.Server({ port: 8080 })
 wss.on("connection", (ws) => {
     console.log("client connected")
-    const adb = process.spawn('adb', ['exec-out', 'screenrecord', '--output-format', 'mp4', '-'])
+    // const adb = process.spawn('adb', ['exec-out', 'screenrecord', '--output-format', 'mp4', '-'])
+
     const ffmpeg = process.spawn('ffmpeg', [
-        '-i', 'pipe:0',
+        // '-i', 'pipe:0',
+        '-i','asset/屏幕录制.mp4',
         '-f', 'mjpeg',
         '-q:v', '5',
         'pipe:1'
     ])
-    adb.stdout.pipe(ffmpeg.stdin)
+    // adb.stdout.pipe(ffmpeg.stdin)
     ffmpeg.stdout.on('data', (data) => {
         ws.send(data)
+        // console.log('sended')
     })
 
     ws.on("close", () => {
-        adb.kill()
+        // adb.kill()
         ffmpeg.kill()
         console.log("client disconnected")
     })
